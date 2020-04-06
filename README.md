440002019461bb5ecbeb6c6884caff4c14dab082
Mariya <alisa-m2@yandex.ru>
2020-04-06 12:02:13 +0300

    Summary

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # idex_test
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Compiles and hot-reloads for development
+```
+npm start
+```
---
+++ build/js/calendar.js
@@ -0,0 +1,87 @@
+/* Employer 1 */
+
+var cal = new CalHeatMap();
+cal.init({
+    itemSelector: "#domainDynamicDimension-one",
+    start: new Date(2019, 0), // January, 1st 2000
+    range: 12,
+    domain: "month",
+    subDomain: "day",
+    cellSize: 8,
+    displayLegend: false,
+    domainDynamicDimension: false,
+    nextSelector: "#domainDynamicDimension-next",
+    previousSelector: "#domainDynamicDimension-previous",
+    itemNamespace: "domainDynamicDimension",
+    tooltip: true,
+    cellPadding: 2,
+});
+
+/* Employer 2 */
+
+var cal = new CalHeatMap();
+cal.init({
+    itemSelector: "#domainDynamicDimension-two",
+    start: new Date(2019, 0), // January, 1st 2000
+    range: 12,
+    domain: "month",
+    subDomain: "day",
+    cellSize: 8,
+    displayLegend: false,
+    domainDynamicDimension: false,
+    nextSelector: "#domainDynamicDimension-next",
+    previousSelector: "#domainDynamicDimension-previous",
+    itemNamespace: "domainDynamicDimension"
+});
+
+/* Employer 3*/
+
+var cal = new CalHeatMap();
+cal.init({
+    itemSelector: "#domainDynamicDimension-three",
+    start: new Date(2019, 0), // January, 1st 2000
+    range: 12,
+    domain: "month",
+    subDomain: "day",
+    cellSize: 8,
+    displayLegend: false,
+    domainDynamicDimension: false,
+    nextSelector: "#domainDynamicDimension-next",
+    previousSelector: "#domainDynamicDimension-previous",
+    itemNamespace: "domainDynamicDimension"
+});
+
+/* Employer 4 */
+
+var cal = new CalHeatMap();
+cal.init({
+    itemSelector: "#domainDynamicDimension-four",
+    start: new Date(2019, 0), // January, 1st 2000
+    range: 12,
+    domain: "month",
+    subDomain: "day",
+    cellSize: 8,
+    displayLegend: false,
+    domainDynamicDimension: false,
+    nextSelector: "#domainDynamicDimension-next",
+    previousSelector: "#domainDynamicDimension-previous",
+    itemNamespace: "domainDynamicDimension"
+});
+
+
+/* Employer 5 */
+
+var cal = new CalHeatMap();
+cal.init({
+    itemSelector: "#domainDynamicDimension-five",
+    start: new Date(2019, 0), // January, 1st 2000
+    range: 12,
+    domain: "month",
+    subDomain: "day",
+    cellSize: 8,
+    displayLegend: false,
+    domainDynamicDimension: false,
+    nextSelector: "#domainDynamicDimension-next",
+    previousSelector: "#domainDynamicDimension-previous",
+    itemNamespace: "domainDynamicDimension"
+});
